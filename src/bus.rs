8e9072eb1c511be28748017df4bddd71a98b5382
hyprd <soulitch7@gmail.com>
2024-03-26 13:37:33 +1300

    Add memory read functions for Bus"

--- src/bus.rs
+++ src/bus.rs
@@ -12,8 +12,38 @@ pub struct Bus {
 }
 
 impl Memory for Bus {
-    fn mem_read(&self, addr: u16) -> u8 {0}
-    fn mem_write(&mut self, addr: u16, value: u8) {}
+    fn mem_read(&self, addr: u16) -> u8 {
+        match addr {
+            RAM_START..=RAM_END => {
+                // CPU has 2KB of RAM which is addressable with 11 bits, but
+                // reserves an address space of 8KB addressable with 13 bits,
+                // therefore requests to memory need to mask bits 12 and 13.
+                let mask = 0b11111111111;
+                self.vram[(addr & mask) as usize]
+            }
+            PPU_START..=PPU_END => {
+                todo!("Implement ppu!");
+            }
+            _ => {
+                println!("Memory access at {:#04X?} ignored", addr);
+                0
+            }
+        }
+    }
+    fn mem_write(&mut self, addr: u16, value: u8) {
+        match addr {
+            RAM_START..=RAM_END => {
+                let mask = 0b11111111111;
+                self.vram[(addr & mask) as usize] = value;
+            }
+            PPU_START..=PPU_END => {
+                todo!("Implement ppu!");
+            }
+            _ => {
+                println!("Memory write at {:#04X?} ignored", addr);
+            }
+        }
+    }
 }
 
 impl Bus {
