6abe564c8213c051eee2203caaeb8a9fb3a4d1c5
hyprd <soulitch7@gmail.com>
2024-05-03 09:45:43 +1200

    Joypad read/write functions added

--- src/joypad.rs
+++ src/joypad.rs
@@ -15,16 +15,35 @@ bitflags! {
 
 pub struct Joypad {
     strobe_status: bool,
-    button_status: u8,
-    button_index: JoypadButton,
+    button_status: JoypadButton,
+    button_index: u8,
 }
 
 impl Joypad {
     pub fn new() -> Self {
         Joypad {
             strobe_status : false,
-            button_status: 0,
-            button_index: JoypadButton::from_bits_truncate(0b00000000),
+            button_index: 0,
+            button_status: JoypadButton::from_bits_truncate(0b00000000),
+        }
+    }
+
+    pub fn read(&mut self) -> u8 {
+        if self.button_index > 7 {
+            return 1;
+        }
+        let result = (self.button_status.bits() & (1 << self.button_index)) >> self.button_index;
+        if !self.strobe_status && self.button_index <= 7 {
+            self.button_index += 1;
+        }
+        result
+    }
+
+    pub fn write(&mut self, value: u8) {
+        // While strobe is high, continuously return state of button A (0b0)
+        if value & 0x01 == 1 {
+            self.strobe_status = true;
+            self.button_index = 0b0;
         }
     }
 }
